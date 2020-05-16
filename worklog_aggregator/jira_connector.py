c7217e66a89374ff2cbb0354dd4f0b7f201f581c
Kenichiro Nishioka <nishioka@3idea.jp>
2020-05-17 08:30:06 +0900

    Use date range to filter worklogs

--- worklog_aggregator/jira_connector.py
+++ worklog_aggregator/jira_connector.py
@@ -14,37 +14,40 @@ def jira_connection():
     return JIRA(options, basic_auth=(os.environ['JIRA_ID'], os.environ['JIRA_TOKEN']))
 
 
-def worklog_dataframe(date):
+def worklog_dataframe(start_date, end_date):
     """Get worklog dataframe updated on `date`.
 
     Args:
-        date(str): worklog updated date.
+        start_date(str): start date of worklog range.
+        end_date (str): end date of worklog range.
 
     Returns:
          pandas.DataFrame: worklog dataframe.
 
     """
-    issue_keys = worklog_updated_issue_keys(date)
+    issue_keys = worklog_updated_issue_keys(start_date, end_date)
+    date_range = pd.date_range(start_date, end_date).strftime('%Y-%m-%d')
     return pd.concat(
         [pd.DataFrame(d) for d in map(extract_issue_worklogs, issue_keys)]
-    ).pipe(lambda df: df[df.updated == date]).reset_index(drop=True)
+    ).pipe(lambda df: df[df.updated.isin(date_range)]).reset_index(drop=True)
 
 
-def worklog_updated_issue_keys(date):
+def worklog_updated_issue_keys(start_date, end_date):
     """Get issues which worklog was updated.
 
     Args:
-        date (str): worklog updated date.
+        start_date (str): start date of worklog range.
+        end_date (str): end date of worklog range.
 
     Retuns
         list of str: issue key list.
 
     Examples
-        >>> worklog_updated_issue_keys('2020-01-01')
+        >>> worklog_updated_issue_keys('2020-01-01', '2020-01-31')
         ['KEY-1', 'KEY-2', 'KEY-3']
 
     """
-    issues = jira_connection().search_issues(f'worklogDate = {date}')
+    issues = jira_connection().search_issues(f"worklogDate >= {start_date} AND  worklogDate <= {end_date}")
     return list(map(lambda x: x.key, issues))
 
 
--- worklog_aggregator/worklog_aggregator.py
+++ worklog_aggregator/worklog_aggregator.py
@@ -8,18 +8,22 @@ from .jira_connector import worklog_dataframe
 plt.rcParams['savefig.bbox'] = 'tight'
 
 
-def main(date):
+def main(start_date, end_date):
     """Create the summary of worklog."""
-    df = worklog_dataframe(date)
+    df = worklog_dataframe(start_date, end_date)
 
-    plt.figure()
-    title = f'Spent hours on tickets on {date}'
-    df.groupby('user').spent_hours.sum().sort_values(ascending=False).plot.bar(title=title)
-    plt.savefig(f'{date}_worklog_summary.png')
+    if not df.empty():
+        plt.figure()
+        title = f'Spent hours on tickets between {start_date} and {end_date}'
+        df.groupby('user').spent_hours.sum().sort_values(ascending=False).plot.bar(title=title)
+        plt.savefig(f'{start_date}-{end_date}_worklog_summary.png')
+    else:
+        print(f'No worklogs between {start_date} and {end_date}.')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--date', help='target date of worklog')
+    parser.add_argument('--start_date', help='target start date of worklog')
+    parser.add_argument('--end_date', help='target end date of worklog')
     args = parser.parse_args()
-    main(args.date)
+    main(args.start_date, args.end_date)
