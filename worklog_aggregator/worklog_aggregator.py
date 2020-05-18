85c5e4abc35c60299857aa730543a140d6d71be4
Kenichiro Nishioka <nishioka@3idea.jp>
2020-05-18 23:57:53 +0900

    Format graphs

--- worklog_aggregator/worklog_aggregator.py
+++ worklog_aggregator/worklog_aggregator.py
@@ -7,22 +7,23 @@ from .jira_connector import worklog_dataframe
 
 plt.rcParams['savefig.bbox'] = 'tight'
 plt.rcParams['figure.figsize'] = 12, 8
+plt.rcParams['font.size'] = 18
 
 
 def main(start_date, end_date):
     """Create the summary of worklog."""
     df = worklog_dataframe(start_date, end_date)
 
-    if not df.empty():
+    if not df.empty:
         plt.figure()
         title = f'Spent hours on tickets between {start_date} and {end_date}'
         df.groupby('user').spent_hours.sum().sort_values(ascending=False).plot.bar(title=title)
         plt.savefig(f'{start_date}-{end_date}_worklog_summary.png')
 
         plt.figure()
-        title = f'Top 10 costing tickets between {start_date} and {end_date}'
+        title = f'Top 10 taking time tickets between {start_date} and {end_date}'
         df.groupby(['issue_key', 'user']).spent_hours.sum().nlargest(10).plot.bar()
-
+        plt.savefig(f'{start_date}-{end_date}_top10_taking_time.png')
     else:
         print(f'No worklogs between {start_date} and {end_date}.')
 
