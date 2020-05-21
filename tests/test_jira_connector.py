597204c9037e53e5d5c4fdc9075c5b3275557613
Kenichiro Nishioka <kenichiro.nishioka@bebit.com>
2020-05-21 18:24:30 +0900

    Add test_worklog_updated_issue_keys

--- tests/test_jira_connector.py
+++ tests/test_jira_connector.py
@@ -1,7 +1,7 @@
 import pytest
 from jira import Issue, Worklog
 
-from worklog_aggregator.jira_connector import worklog_to_dict, extract_issue_worklogs
+from worklog_aggregator.jira_connector import worklog_to_dict, extract_issue_worklogs, worklog_updated_issue_keys
 
 
 def raw_worklog():
@@ -55,6 +55,20 @@ def jira_issue():
     return Issue(None, None, raw=raw)
 
 
+def test_worklog_updated_issue_keys(jira_issue, mocker):
+    issues_mock = mocker.Mock()
+    issues_mock.search_issues.return_value = [jira_issue]
+    mocker.patch('worklog_aggregator.jira_connector.jira_connection').return_value = issues_mock
+    assert worklog_updated_issue_keys('2020-03-01', '2020-03-31') == ['KEY-1']
+
+
+def test_extract_issue_worklogs(jira_issue, mocker):
+    issue_mock = mocker.Mock()
+    issue_mock.issue.return_value = jira_issue
+    mocker.patch('worklog_aggregator.jira_connector.jira_connection').return_value = issue_mock
+    assert extract_issue_worklogs('2020-01-01')[0]['spent_hours'] == 6.0
+
+
 def test_worklog_to_dict(jira_worklog):
     expected_dict = {
         'spent_hours': 6.0,
@@ -63,10 +77,3 @@ def test_worklog_to_dict(jira_worklog):
         'user': 'user1'
     }
     assert worklog_to_dict(jira_worklog) == expected_dict
-
-
-def test_extract_issue_worklogs(jira_issue, mocker):
-    issue_mock = mocker.Mock()
-    issue_mock.issue.return_value = jira_issue
-    mocker.patch('worklog_aggregator.jira_connector.jira_connection').return_value = issue_mock
-    assert extract_issue_worklogs('2020-01-01')[0]['spent_hours'] == 6.0
