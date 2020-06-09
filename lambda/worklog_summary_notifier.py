aa275cb9d4c4a462a3d8447aedab1351d367bb20
Kenichiro Nishioka <kenichiro.nishioka@bebit.com>
2020-06-09 17:26:38 +0900

    Add empty lambda function

---
+++ lambda/worklog_summary_notifier.py
@@ -0,0 +1,7 @@
+"""Lambda function to notify the summary of worklog."""
+
+
+def worklog_handler(event, context):
+    """Handler to create and notify the summary of worklogs."""
+
+    print('Notify the summary of worklogs.')
