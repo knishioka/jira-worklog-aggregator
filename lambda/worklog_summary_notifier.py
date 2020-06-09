a781bd7629af355efc01efb7562efab0866cfb11
Kenichiro Nishioka <kenichiro.nishioka@bebit.com>
2020-06-09 18:06:16 +0900

    Fix flake8-docstrings

--- lambda/worklog_summary_notifier.py
+++ lambda/worklog_summary_notifier.py
@@ -2,6 +2,5 @@
 
 
 def worklog_handler(event, context):
-    """Handler to create and notify the summary of worklogs."""
-
+    """Notify the summary of worklogs."""
     print('Notify the summary of worklogs.')
